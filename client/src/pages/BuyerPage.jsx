58e2e3175fb0cd0d4bc3a2b07d9f74af43bc281e
Aishwarya <Aishwaryalodhi2014@gmail.com>
2022-10-31 23:08:45 -0400

    Changes based on FavItems and changed its views

--- client/src/pages/BuyerPage.jsx
+++ client/src/pages/BuyerPage.jsx
@@ -9,27 +9,42 @@ import { useEffect } from "react";
 
 export default function BuyerPage() {
   const [items, setItems] = useState([]);
+  const [likedItems, setLikedItems] = useState([]);
+
   useEffect(() => {
-    let data;
-    axios
-      .get("/item")
+    axios.get("/item")
       .then((res) => {
-        data = res.data;
+        let data = res.data;
         setItems(data)
       })
       .catch((err) => {});
-  });
+      getUserItems()
+  },[likedItems]);
+
+const getUserItems = () =>{
+  axios.get("/favitem?userId=Tom")
+    .then(res=>{
+        let data = res.data[0];
+        //data has fitemId array and userID
+        setLikedItems(data.fitemId)
+    })
+    .catch(err=>console.log(err))
+}
 
   function create(item) {
+    let isLiked = likedItems.filter(i => i == item.itemId).length > 0 ? true : false
+    
     return (
       <ItemCard
-        key={item.id}
+        key={item.itemId}
+        id={item.itemId}
         price={item.price}
         name={item.name}
         condition={item.condition}
         filename={item.filename}
         ph={item.ph}
         address={item.address}
+        isLiked={isLiked}
       />
     );
   }
