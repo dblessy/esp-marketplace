1b154ee3209901e2f74d3c33b05b42e345d9a93d
nisha-sjsu <srinishaa.prabhakaran@sjsu.edu>
2022-11-24 10:29:34 -0800

    favorites bug fixed

--- client/src/pages/BuyerPage.jsx
+++ client/src/pages/BuyerPage.jsx
@@ -3,42 +3,47 @@ import React, { useState } from "react";
 import Search from "../components/Search";
 import "../public/styles/buyerPage.css";
 import { Navbar, NavbarBrand } from "reactstrap";
-import dummyItems from "../items";
 import ItemCard from "../components/ItemCard";
 import { useEffect } from "react";
 import Login from "./Login";
 
 export default function BuyerPage() {
   const [items, setItems] = useState([]);
-  const [likedItems, setLikedItems] = useState([]);
+  const [likedItems, setLikedItems] = useState();
 
   useEffect(() => {
-    axios.get("/item")
-      .then((res) => {
-        let data = res.data;
-        setItems(data)
-      })
-      .catch((err) => {});
-      getUserItems()
-  },[likedItems]);
+    getUserItems();
+    if (likedItems) {
+      axios
+        .get("/item")
+        .then((res) => {
+          let data = res.data;
+          setItems(data);
+        })
+        .catch((err) => {});
+    }
+  }, [likedItems]);
 
-  
-const getUserItems = () =>{
-  let user = (localStorage.getItem('userEmail'))
-  axios.get("/favitem?userId="+user)
-  // axios.get("/favitem")   
-    .then(res=>{
+  const getUserItems = () => {
+    let user = localStorage.getItem("userEmail");
+    axios
+      .get("/favitem?userId=" + user)
+      // axios.get("/favitem")
+      .then((res) => {
         let data = res.data[0];
-        console.log(data)
+        if (data == undefined) {
+          setLikedItems([]);
+        }
         //data has fitemId array and userID
-        setLikedItems(data.fitemId)
-    })
-    .catch(err=>console.log(err))
-}
+        else setLikedItems(data.fitemId);
+      })
+      .catch((err) => console.log(err));
+  };
 
   function create(item) {
-    let isLiked = likedItems.filter(i => i == item.itemId).length > 0 ? true : false
-    
+    let isLiked =
+      likedItems.filter((i) => i == item.itemId).length > 0 ? true : false;
+
     return (
       <ItemCard
         key={item.itemId}
@@ -62,7 +67,7 @@ const getUserItems = () =>{
           <span>
             <a href="/favorites">Favorites</a>
             &nbsp;&nbsp;
-            <Login/>
+            <Login />
           </span>
         </NavbarBrand>
       </Navbar>
