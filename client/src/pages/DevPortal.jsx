e2c1b965c5f531d1b2bed6ab06180d49c140236f
nisha-sjsu <srinishaa.prabhakaran@sjsu.edu>
2022-11-28 17:59:38 -0800

    dev page enhanced

--- client/src/pages/DevPortal.jsx
+++ client/src/pages/DevPortal.jsx
@@ -3,44 +3,86 @@ import { faGithubAlt, faJenkins } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useOktaAuth } from "@okta/okta-react";
 import useAuthUser from "../components/User";
-import {Container, Navbar, NavbarBrand, Button} from "reactstrap";
+import { Container, Navbar, NavbarBrand, Button } from "reactstrap";
 
 export default function DevPortal() {
-    const userInfo = useAuthUser();
-    const { oktaAuth, authState } = useOktaAuth();
-    const loggingIn = async () => oktaAuth.signInWithRedirect({ originalUri: "/" });
-    const loggingOut = async () => oktaAuth.signOut();
+  const userInfo = useAuthUser();
+  const { oktaAuth, authState } = useOktaAuth();
+  const loggingIn = async () =>
+    oktaAuth.signInWithRedirect({ originalUri: "/" });
+  const loggingOut = async () => oktaAuth.signOut();
 
-    return (
-      <Container>
-          <Navbar color="light" light expand="md">
-              <NavbarBrand href="/">ESP Marketplace</NavbarBrand>
-              {
-                  authState?.isAuthenticated ? (
-                      <Button variant="outline-success" onClick={loggingOut}>Logout</Button>
-                  ) : (
-                      <Button variant="outline-success" onClick={loggingIn}>Login</Button>
-                  )
-              }
-          </Navbar>
+  return (
+    <Container>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">ESP Marketplace</NavbarBrand>
+        {authState?.isAuthenticated ? (
+          <Button variant="outline-success" onClick={loggingOut}>
+            Logout
+          </Button>
+        ) : (
+          <Button variant="outline-success" onClick={loggingIn}>
+            Login
+          </Button>
+        )}
+      </Navbar>
       {authState?.isAuthenticated ? (
-    <>
-      <h3 style={{ textAlign: "center", margin: "20px" }}>
-        Welcome {userInfo?.name}!
-      </h3>
-      <div style={{justifyContent: "space-around", marginTop:"20px"}}>
-        <a href="#" style={{color : "black"}}>
-          <FontAwesomeIcon icon={faGithubAlt} size="4x" />
-        </a>
-        <a href="#" style={{color : "black"}}>
-          <FontAwesomeIcon icon={faJenkins} size="4x" />
-        </a>
-      </div>
-    </>) : (
-          <p style={{ textAlign: "center", marginTop: "6rem", fontSize: '2rem' }}>
-              Please login to proceed
-          </p>
+        <>
+          <h3 style={{ textAlign: "center", margin: "20px" }}>
+            Welcome {userInfo?.name}!
+          </h3>
+          <div
+            style={{ textAlign: "center", margin: "10px", marginTop: "20px" }}
+          >
+            <a
+              href="https://github.com/TechWizards-ESPMarketplace/esp-marketplace"
+              style={{ color: "black" }}
+            >
+              <Button style={{ margin: "20px" }}>
+                <FontAwesomeIcon icon={faGithubAlt} size="6x" />
+              </Button>
+            </a>
+            <a href="jenkins.127-0-0-1.sslip.io" style={{ color: "black" }}>
+              <Button style={{ margin: "20px" }}>
+                <FontAwesomeIcon icon={faJenkins} size="6x" />
+              </Button>
+            </a>
+            <a href="grafana.127-0-0-1.sslip.io">
+              <Button style={{ margin: "20px" }}>
+                <img
+                  src="https://www.vectorlogo.zone/logos/grafana/grafana-icon.svg"
+                  style={{ height: "90px" }}
+                  alt=""
+                />
+              </Button>
+            </a>
+
+            <a href="alertmanager.127-0-0-1.sslip.io">
+              <Button style={{ margin: "20px" }}>
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-256/prometheus-282488.png"
+                  style={{ height: "90px" }}
+                  alt=""
+                />
+              </Button>
+            </a>
+            <a href="prometheus.127-0-0-1.sslip.io">
+              <Button style={{ margin: "20px" }}>
+                <img
+                  src="https://cdn.iconscout.com/icon/free/png-256/prometheus-282488.png"
+                  style={{ height: "90px" }}
+                  alt=""
+                />
+                Alert manager
+              </Button>
+            </a>
+          </div>
+        </>
+      ) : (
+        <p style={{ textAlign: "center", marginTop: "6rem", fontSize: "2rem" }}>
+          Please login to proceed
+        </p>
       )}
-          </Container>
+    </Container>
   );
 }
